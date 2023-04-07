8e2aff304130c37b39f72ac9d6ceec9b602515c5
Muhammad Tarek <mohammadtarek000@gmail.com>
2023-04-07 16:56:28 +0200

    all works except dropdown

--- src/logic/extracting.py
+++ src/logic/extracting.py
@@ -10,12 +10,22 @@ from bs4 import BeautifulSoup
 def isHindi(character) -> bool:
     import unicodedata
     for c in character:
-        # if ord(c) >= 2304 and ord(c) <= 2431:
-        #     return True
         if unicodedata.name(c).startswith("DEVANAGARI") and unicodedata.category(c) == "Lo":
             return True
     return False
 
+def isHighResolution(image) -> bool:
+    return image.find("blur") == -1
+    # TODO: remove
+    import requests
+    from io import BytesIO
+    from PIL import Image
+    response = requests.get(image)
+    img = Image.open(BytesIO(response.content))
+    width, height = img.size
+    if width >= 500 and height >= 500:
+        return True
+    return False
 
 
 def extract_data(url):
@@ -39,6 +49,7 @@ def extract_data(url):
 class Extractor:
     def __init__(self) -> None:
         self.driver: webdriver.Chrome = self.getDriver()
+        self.waiter: WebDriverWait = WebDriverWait(self.driver, 10)
         self.url: str = None
         self.innerUrls: list = None
         self.pageRight: bool = True
@@ -63,27 +74,58 @@ class Extractor:
     def startTesting(self, data: list) -> None:
         for url in data:
             self.url = url
+            self.output[self.url] = []
             if not self.testPage():
-                self.output[self.url] = "Page not working"
+                self.output[self.url] = "Wrong Page"
                 continue
-            elif not self.testWorkingButtons():
-                self.output[self.url] = "Buttons not working"
-                continue
-            elif not self.testImagesResolutions():
-                self.output[self.url] = "Images not working"
-                continue
-            elif not self.testHindiUrls():
-                self.output[self.url] = "Inner pages not translated"
-                continue
+            if not self.testWorkingButtons():
+                self.output[self.url].append("Javascript dropdown not working properly")
+            if not self.testImagesResolutions():
+                self.output[self.url].append("Images not high resolution")
+            if not self.testHindiUrls():
+                self.output[self.url].append("Inner pages not translated")
 
     def testPage(self) -> bool:
-        if self.url:
+        self.driver.get(self.url)
+        print(f"{self.url}: Page")
+        try:
+            self.driver.find_element(By.XPATH, '//*[@class="symbol-classcentral-navy symbol-medium hidden large-up-block"]')
             return True
+        except:
+            return False
 
     def testWorkingButtons(self) -> bool:
+        # TODO: still working
+        self.driver.get(self.url)
+        print(f"{self.url}: Buttons")
+        return True
+        try:
+            button1 = self.driver.find_element(By.XPATH, '//*[@id="page-home"]/div[1]/header/div[1]/nav/div[1]/button[1]')
+            button2 = self.driver.find_element(By.XPATH, '//*[@id="page-home"]/div[1]/header/div[1]/nav/div[2]/a')
+            try:
+                button1.click()
+                # //*[@id="page-home"]/div[1]/header/div[1]/nav/div[1]/nav
+            except:
+                pass
+            try:
+                # //*[@id="page-home"]/div[1]/header/div[1]/nav/div[2]/div
+                pass
+            except:
+                pass
+        except:
+            return False
         return True
 
     def testImagesResolutions(self) -> bool:
+        self.driver.get(self.url)
+        print(f"{self.url}: Image")
+        try:
+            images = self.driver.find_elements(By.XPATH, '//img')
+            for image in images:
+                if not isHighResolution(image.get_attribute("src")):
+                    return False
+        except:
+            pass
         return True
 
     def testHindiUrls(self) -> bool:
@@ -92,22 +134,22 @@ class Extractor:
             self.soup = BeautifulSoup(self.driver.page_source, "html.parser")
             if self.getTextAndCheck():
                 self.innerUrls = [a["href"] for a in self.soup.find_all("a", href=True)]
-                self.innerUrls = [url for url in self.innerUrls if url.startswith("/")]
                 return self.checkInner()
 
-    def getTextAndCheck(self):
+    def getTextAndCheck(self)->bool:
         allText = filter(None, self.soup.get_text().split("\n"))
         for text in allText:
             if isHindi(text):
                 return True
         return False
 
-    def checkInner(self):
+    def checkInner(self)->bool:
         flag = True
         if self.innerUrls:
             import random
             for inner in random.choices(self.innerUrls, k=4):
                 url = f"{self.url}{inner}"
+                print(f"{self.url}: Hindi")
                 self.driver.get(url)
                 self.soup = BeautifulSoup(self.driver.page_source, "html.parser")
                 if not self.getTextAndCheck():
@@ -121,6 +163,8 @@ data = [
     "https://ammardab3an99.github.io/",
     "https://sinistersup.github.io/classcentral-hindi/",
     "https://class-central.vercel.app/www.classcentral.com/index.html",
+    "https://www.afternic.com/forsale/NA.CO?utm_source=TDFS&utm_medium=sn_affiliate_click&utm_campaign=TDFS_Affiliate_namefind_direct7&traffic_type=CL3&traffic_id=Namefind"
+    "https://sinistersup.github.io/classcentral-hindi/",
 ]
 
 if __name__ == "__main__":
